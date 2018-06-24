34fdacb2b8419354223b1f97d82874e2ca4209fd
Bruno Klein Salvalaio <brunoklein99@gmail.com>
2018-06-24 10:29:34 -0300

    adds settings and proper data transformation

--- data_load.py
+++ data_load.py
@@ -1,5 +1,9 @@
+import datetime
+
 import pandas as pd
 import numpy as np
+from dateutil.relativedelta import relativedelta
+from math import pi, sin, cos
 
 
 def count_positive(v):
@@ -23,80 +27,87 @@ def get_keep_indexes(x):
     return indexes
 
 
-# http://isiarticles.com/bundles/Article/pre/pdf/20710.pdf (reference from DeepAR)
-# end of page 1, right below abstract
+def sample(time_offset, series, gran='monthly', seq_len=8, debug=False):
+    assert len(time_offset) == len(series)
+    N, T = series.shape
+    X = []
+    Z = []
+    for i in range(N):
+        if debug:
+            print('i =', i + 1)
+            print('z_{i, 1:T}  : ', list(series[i]))
+        for t_window in range(T - seq_len + 1):
+            if debug:
+                print('t_window: ', t_window)
+            x = []
+            z = []
+            for t_time_step in range(seq_len):
+
+                f = []
+
+                t = t_window + t_time_step
+
+                z_prev = 0.0
+                z_curr = series[i, t]
+                if t > 0:
+                    z_prev = series[i, t - 1]
+
+                f.append(z_prev)
+
+                if debug:
+                    print('t_time_step : ', t_time_step)
+                    print('t           : ', t)
+                    print('z_{i, t - 1}: ', z_prev)
+                    print('z_{i, t}    : ', z_curr)
+
+                f.append(t / (T - 1))
+                f.append(t_time_step / (seq_len - 1))
+
+                if gran == 'monthly':
+                    d = time_offset[i] + relativedelta(months=t)
+                    f.append(sin(2 * pi * ((d.month - 1) / 11)))
+                    f.append(cos(2 * pi * ((d.month - 1) / 11)))
+                else:
+                    raise Exception('unknown gran')
+
+                x.append(f)
+                z.append([z_curr])
+
+            X.append(x)
+            Z.append(z)
+
+    X = np.array(X)
+    Z = np.array(Z)
+
+    return X, Z
+
+
 def load_parts(debug=False):
     df = pd.read_csv('data/carparts.csv')
 
-    # "out of 2509 series with complete records for 51 months"
     df = df.dropna()
 
-    parts = df.values
-    parts = parts[:, 1:]
+    p = df.values
+    p = p[:, 1:]
 
-    indexes = get_keep_indexes(parts)
-    parts = parts[indexes]
-    # parts /= np.max(parts)
+    indexes = get_keep_indexes(p)
 
-    # x and y are the same time series, with y shifted by 1
-    # carparts dataset is composed of 51 months, minus 1 for x, y shifting
-    x = parts[:, :50]
-    y = parts[:, 1:51]
+    p = p[indexes]
 
-    assert x.shape == y.shape
+    z = p
 
-    # last 8 months are used for validation
-    valid_x = x[:, -8:]
-    # from these 8, the first 4 are sequence encoding (past) and the last 4 are predicting (future)
-    valid_y = y[:, -4:]
-    # add feature dimension to decoder output
-    valid_y = np.expand_dims(valid_y, axis=-1)
+    z = z[:, 1:51]
 
-    # as above, first 4 months are the encoder inp
-    enc_x_valid = valid_x[:, :4]
-    # as above, last 4 months are decoder inp
-    dec_x_valid = valid_x[:, -4:]
+    assert len(z) == len(p)
 
-    # add feature dimension
-    enc_x_valid = np.expand_dims(enc_x_valid, axis=-1)
-    dec_x_valid = np.expand_dims(dec_x_valid, axis=-1)
+    if debug:
+        for i in range(len(z)):
+            print('{}th sample'.format(i + 1))
+            print('p ', list(p[i]))
+            print('z ', list(z[i]))
 
-    # tmp, hold all training related data
-    train_x = []
-    train_y = []
+    z = z[:, :42]
 
-    # first 42 months are used for training, a training sample is generated
-    # from each 8 month window_x
-    x = np.expand_dims(x[:, :42], axis=-1)
-    y = np.expand_dims(y[:, :42], axis=-1)
+    time_offset = [datetime.date(1998, 1, 1)] * len(z)
 
-    # for each sequence
-    for i in range(len(x)):
-        if debug:
-            print('parts     : ', list(np.squeeze(parts[i])))
-            print('parts     : ', list(np.squeeze(parts[i])))
-            print('sequence x: ', list(np.squeeze(x[i])))
-            print('sequence y: ', list(np.squeeze(y[i])))
-        # for each 8 month sequence
-        for j in range(42 - 8 + 1):
-            # adds 8 months to list
-            if debug:
-                print('subseq ', j)
-            window_x = x[i, j:j+8]
-            window_y = y[i, j+4:j+8]
-            if debug:
-                print('x: ', list(np.squeeze(window_x)))
-                print('y:                     ', list(np.squeeze(window_y)))
-            train_x.append(np.expand_dims(window_x, axis=0))
-            train_y.append(np.expand_dims(window_y, axis=0))
-
-    train_x = np.concatenate(train_x, axis=0)
-    train_y = np.concatenate(train_y, axis=0)
-
-    assert len(train_x) == len(train_y)
-
-    # same as above
-    enc_x_train = train_x[:, :4]
-    dec_x_train = train_x[:, -4:]
-
-    return enc_x_train, dec_x_train, train_y, enc_x_valid, dec_x_valid, valid_y
+    return sample(time_offset, z, seq_len=8, debug=debug)
