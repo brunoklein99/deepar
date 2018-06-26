cbdb65067987cee3f6af5b57597c9a410ed78f8f
Bruno Klein Salvalaio <brunoklein99@gmail.com>
2018-06-26 14:25:24 -0300

    removes misc stuff

--- train.py
+++ train.py
@@ -13,26 +13,6 @@ def gamma(x):
     return torch.exp(torch.lgamma(x))
 
 
-def neg_bin_pdf(r, k, m):
-    t1_n = gamma(r + k)
-    t1_d = gamma(k + 1) * gamma(r)
-    t1 = t1_n / t1_d
-
-    t2_n = m
-    t2_d = r + m
-    t2 = t2_n / t2_d
-    t2 = torch.pow(t2, k)
-
-    t3_n = r
-    t3_d = r + m
-    t3 = t3_n / t3_d
-    t3 = torch.pow(t3, r)
-
-    pdf = t1 * t2 * t3
-
-    return pdf
-
-
 def neg_bin_loss(z, m, a):
 
     inva = 1 / a
@@ -52,13 +32,7 @@ def neg_bin_loss(z, m, a):
     t3 = t3_n / t3_d
     t3 = torch.pow(t3, z)
 
-    # pdf = neg_bin_pdf(
-    #     r=1/a,
-    #     k=z,
-    #     m=m,
-    # )
     pdf = t1 * t2 * t3
-    pdf[pdf > 100] = 100
 
     loss = torch.log(pdf)
     loss = torch.sum(loss)
