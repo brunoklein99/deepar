bfa1c307ada245442c1e81347f304fc69aae2741
Bruno Klein Salvalaio <brunoklein99@gmail.com>
2018-06-28 20:03:52 -0300

    adds valid sagemaker file

--- sagemaker.py
+++ sagemaker.py
@@ -1,16 +1,20 @@
 import json
 from data_load import get_parts_series
 
-if __name__ == '__main__':
 
-    datetime_offset, s = get_parts_series()
-    s = s[:, :-8]
-
-    with open('data/sagemaker_train.json', 'w') as f:
+def write_file(filename, datetime_offset, s):
+    with open(filename, 'w') as f:
         for i in range(len(s)):
             f.write(json.dumps({
                 'start': str(datetime_offset),
                 'target': list(s[i])
-            })+'\n')
+            }) + '\n')
+
+
+if __name__ == '__main__':
+    datetime_offset, s = get_parts_series()
+
+    write_file('data/sagemaker_train.json', datetime_offset, s[:, :-8])
+    write_file('data/sagemaker_valid.json', datetime_offset, s[:, -8:])
 
     print('finished')
