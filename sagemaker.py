d59bd62ce4d4a8e0b4afdad4ae0f123ea5d2cb18
Bruno Klein Salvalaio <brunoklein99@gmail.com>
2018-06-30 12:24:02 -0300

    sagemaker data

--- sagemaker.py
+++ sagemaker.py
@@ -1,4 +1,7 @@
 import json
+
+from dateutil.relativedelta import relativedelta
+
 from data_load import get_parts_series
 
 
@@ -12,9 +15,16 @@ def write_file(filename, datetime_offset, s):
 
 
 if __name__ == '__main__':
-    datetime_offset, s = get_parts_series()
+    offset_train, s = get_parts_series()
 
-    write_file('data/sagemaker_train.json', datetime_offset, s[:, :-8])
-    write_file('data/sagemaker_valid.json', datetime_offset, s)
+    _, T = s.shape
+
+    enc_len = 8
+    dec_len = 8
+
+    offset_valid = offset_train + relativedelta(months=T - dec_len - enc_len)
+
+    write_file('data/sagemaker_train.json', offset_train, s[:, :-8])
+    write_file('data/sagemaker_valid.json', offset_valid, s[:, -16:])
 
     print('finished')
