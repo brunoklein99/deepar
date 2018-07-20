fb574e05d3c43b2d8fd97e5b47ef587d9ba56018
bruno klein <brunoklein99@gmail.com>
2018-07-20 22:40:02 +0000

    converging

--- train.py
+++ train.py
@@ -185,12 +185,13 @@ if __name__ == '__main__':
 
                 if i % 100 == 0:
                     print('fold {} epoch {} batch {}/{} loss: {}'.format(nfold, epoch, i, len(loader), loss))
-            z = pred(model, enc_x, enc_z, dec_x, v_valid)
-            metric = smape(z, dec_z)
-            print('smape', metric)
-            dirname = 'checkpoints/{}'.format(nfold)
-            makedirs(dirname, exist_ok=True)
-            save_model('{}/checkpoint-{:2f}'.format(dirname, metric), model)
+            if epoch % 5 == 0:
+                z = pred(model, enc_x, enc_z, dec_x, v_valid)
+                metric = smape(z, dec_z)
+                print('smape', metric)
+                dirname = 'checkpoints/{}'.format(nfold)
+                makedirs(dirname, exist_ok=True)
+                save_model('{}/checkpoint-{:2f}'.format(dirname, metric), model)
 
         del model
         del x_valid
